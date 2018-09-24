6623fb28e39062bc15bedde1b2626801cd38d546
Lina Yang <t886515@hotmail.com>
2018-09-24 02:24:41 -0500

    done without calendar icon

--- index.js
+++ index.js
@@ -7,46 +7,92 @@ function init() {
     console.log('im here');
     $("#get-involved-btn").on("click", function(evt) {
         evt.preventDefault();
-        const containerTitle = $("#container-title");
-        const containerContent = $('#container-content-1');
-        const containerWrapper = $('#container-wrapper');
-        containerTitle.text('Thank you');
+        $("#container-title")
+            .text('Thank you')
+            .addClass('text-center');
 
         // select title, update title text
         // select content, update content text
-
-        containerContent.text('You have just taken a huge step in making an environmental difference! We are excited to have you on board so we can keep progressing towards getting Fayette taken down.');
-        containerWrapper.addClass('text-center');
-
-        const shareTemplate = $('#container-content-2').add('span');
-        $('#sign-up-form').remove();
-        shareTemplate.addClass('text-center');
-        shareTemplate.text('Check your inbox in a few minutes. We are sending you details about our next strategic meeting!')
         
-        const shareAndCalendar = this.composeShareAndCalendar();
-        shareTemplate.append(shareAndCalendar);
-
+        $('#container-content-1')
+            .addClass('text-center');
+        $('#container-content-1 span')
+            .text('You have just taken a huge step in making an environmental difference! We are excited to have you on board so we can keep progressing towards getting Fayette taken down.')
+       
         // select form, remove form
         // append the last paragraph with icons and back to home anchor ele
+        $('#sign-up-form').remove();
+        $('#container-content-2')
+            .addClass('text-center')
+            .append('<span></span>');
+        $('#container-content-2 span')
+            .text('Check your inbox in a few minutes. We are sending you details about our next strategic meeting!')
         
+        const shareAndCalendarTemplate = window.composeShareAndCalendar();
+        shareAndCalendarTemplate
+            .addClass('col-12 my-3 text-center justify-content-center')
+            
+
+        $('#container-wrapper')
+            .append(shareAndCalendarTemplate)
+            .append('<div class="text-center"><a style="font-size: 0.8rem; text-decoration: underline;">Back To Home</a></div>');
+        
+        window.addShareEvent();
+            
+      
         //select the picture, update the picture src from original to new one
+        $('#bottom-img')
+            .attr('src', 'images/350Austin-divest.jpg')
     })
 }
 
 function composeShareAndCalendar() {
-    
+    $shareAndCalendar = $('<div></div>');
+    const btns = ['share', 'add-to-calendar'];
+    btns.forEach(txt => {
+        const cleanTxt = txt.split(' ').join('') 
+        $shareAndCalendar
+            .append(`<button><img src="images/${txt}.png" style="width: 30px;"></button>`)
+            .find('button:not(.share-btns)')
+            // .html(txt)
+            .attr('id', cleanTxt)
+            .addClass('btn mx-1 share-btns');
+    });
+    return $shareAndCalendar;
 }
 
 function composeShareOptions() {
+    const options = ['instagram', 'facebook', 'twitter', 'google'];
+    $shareOptions = $('<span></span>')
+        .attr('id', 'share-opts')
 
+    options.forEach(txt => {
+      
+        $shareOptions
+            .append(`<img src="images/${txt}.png">`)
+            .find('img:not(.share-opt)')
+            // .html(txt)
+            .attr('id', txt)
+            .css('width', '30px')
+            .addClass('mx-1 share-opt');
+    })
+    return $shareOptions;
 }
 
 
 
 function addShareEvent() {
-    $('#share-icon').on('click', function(evt) {
+    let clicked = false;
+    $('#share').on('click', function(evt) {
         evt.preventDefault();
-
-    })
+        if (clicked) {
+            $('#share-opts').remove();
+            $("#AddToCalendar").html('Add To Calendar');
+            clicked = false;
+        } else {
+            $(this).after(window.composeShareOptions());
+            $("#AddToCalendar").html('Cal');
+            clicked = true;
+        }
+    })   
 }
-
